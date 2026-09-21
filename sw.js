71a5e0a9df539795f2fb7e845385a3a7ea8ba361
juankahf24 <juanka.hf.24@gmail.com>
2026-09-21 19:41:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-/* MILITOPO PWA · v2-e1-account-visible-20260921 · shell estable y librerías críticas en caché */
-const CACHE_NAME="militopo-v2-pwa-v2-e1-account-visible-20260921";
-const RUNTIME_CACHE="militopo-v2-pwa-runtime-v2-e1-account-visible-20260921";
-const APP_SHELL=["./","./index.html","./styles.css","./styles.css?v=v2-e1-account-visible-20260921","./app.js","./app.js?v=v2-e1-account-visible-20260921","./manifest.webmanifest","./icons/militopo-192.png","./icons/militopo-512.png","./icons/militopo-startup-1536.png","./icons/militopo-startup-premium-2048x3072.jpg","./js/v2/firebase-config.js","./js/v2/bootstrap.js","./js/v2/auth/roles.js","./js/v2/auth/auth-ui.css?v=v2-e1-account-visible-20260921","./js/v2/auth/auth-ui.js?v=v2-e1-account-visible-20260921","./js/v2/data/invitation-inbox.js?v=v2-e1-account-visible-20260921"];
+/* MILITOPO PWA · v2-e2-usernames-bulk-20260921 · shell estable y librerías críticas en caché */
+const CACHE_NAME="militopo-v2-pwa-v2-e2-usernames-bulk-20260921";
+const RUNTIME_CACHE="militopo-v2-pwa-runtime-v2-e2-usernames-bulk-20260921";
+const APP_SHELL=["./","./index.html","./styles.css","./styles.css?v=v2-e2-usernames-bulk-20260921","./app.js","./app.js?v=v2-e2-usernames-bulk-20260921","./manifest.webmanifest","./icons/militopo-192.png","./icons/militopo-512.png","./icons/militopo-startup-1536.png","./icons/militopo-startup-premium-2048x3072.jpg","./js/v2/firebase-config.js","./js/v2/bootstrap.js","./js/v2/auth/roles.js","./js/v2/auth/auth-ui.css?v=v2-e2-usernames-bulk-20260921","./js/v2/auth/auth-ui.js?v=v2-e2-usernames-bulk-20260921","./js/v2/data/invitation-inbox.js?v=v2-e2-usernames-bulk-20260921"];
 const REMOTE_ASSETS=[
 "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css","https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
 "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png","https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png","https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
